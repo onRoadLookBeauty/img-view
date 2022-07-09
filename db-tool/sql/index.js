380586bcce811d8957aa4edfa1b5f9970f12e4d5
yanbin <1105660821@qq.com>
2022-07-09 14:31:03 +0800

    1

--- db-tool/sql/index.js
+++ db-tool/sql/index.js
@@ -3,7 +3,7 @@ var mysql = require('mysql')
 // 数据库连接池的配置
 var pool = mysql.createPool({
   connectionLimit: 10,  // 连接池的大小
-  host: '192.168.20.35', // 主机名
+  host: 'localhost', // 主机名
   user: 'db-tool',  // 用户名
   password: '123456', // 密码
   database: 'my-tool' // 数据库名称 在数据库里面建立了一个my-tool数据库，里面有很多表格
